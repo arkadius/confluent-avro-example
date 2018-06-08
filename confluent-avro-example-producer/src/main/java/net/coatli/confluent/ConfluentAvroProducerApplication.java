b891380a8257006f80a81f442326f7ba3bfa88b5
Arek Burdach <arek.burdach@gmail.com>
2018-06-08 15:21:01 +0200

    customized fields, elements and batch length

--- confluent-avro-example-producer/src/main/java/net/coatli/confluent/ConfluentAvroProducerApplication.java
+++ confluent-avro-example-producer/src/main/java/net/coatli/confluent/ConfluentAvroProducerApplication.java
@@ -11,56 +11,58 @@ import org.apache.avro.generic.GenericRecord;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.ProducerConfig;
 
 import net.coatli.confluent.domain.Item;
 
 public class ConfluentAvroProducerApplication {
 
-  private static final int TOTAL_ITEMS = 40;
-
-  private static final List<Item> items = new ArrayList<>(TOTAL_ITEMS);
-
-  static {
-    for (int index = 0; index < TOTAL_ITEMS; index++) {
-      items.add(
-        new Item()
-          .setKey(index)
-          .setDescription("Chifladera " + index)
-          .setCheckInDate(Calendar.getInstance().getTime()));
-    }
-  }
-
   public static void main(final String[] args) {
+
+    int FIELDS = Integer.parseInt(args[0]);
+    int TOTAL_ITEMS = Integer.parseInt(args[1]);
+    int BATCH = Integer.parseInt(args[2]);
+    int THREADS = Integer.parseInt(args[3]);
 
     final Properties props = new Properties();
     props.put("bootstrap.servers", "localhost:9092");
-    props.put("acks", "all");
-    props.put("retries", 0);
     props.put("key.serializer", "io.confluent.kafka.serializers.KafkaAvroSerializer");
     props.put("value.serializer", "io.confluent.kafka.serializers.KafkaAvroSerializer");
     props.put("schema.registry.url", "http://localhost:8081");
+    props.put(ProducerConfig.LINGER_MS_CONFIG, BATCH);
+    
     final Producer<Integer, GenericRecord> producer = new KafkaProducer<Integer, GenericRecord>(props);
+
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < FIELDS; i++) {
+      sb.append("{\"name\": \"field");
+      sb.append(i);
+      sb.append("\", \"type\": \"int\"}");
+      if (i != FIELDS-1)
+        sb.append(",");
+    }
 
     final String schemaString =
                             "{\"type\": \"record\", " +
                             "\"name\": \"item\"," +
                             "\"fields\": [" +
-                            "{\"name\": \"id\", \"type\": \"int\"}," +
-                            "{\"name\": \"description\", \"type\": \"string\"}," +
-                            "{\"name\": \"checkin\", \"type\": \"long\"}" +
+                            sb +
                             "]}";
     final Schema schema = new Schema.Parser().parse(schemaString);
 
-    for (final Item item : items) {
-      final GenericRecord record = new GenericData.Record(schema);
-      record.put("id", item.getKey());
-      record.put("description", item.getDescription());
-      record.put("checkin", item.getCheckInDate().getTime());
 
-      producer.send(new ProducerRecord<Integer, GenericRecord>("items", item.getKey(), record));
+    final GenericRecord record = new GenericData.Record(schema);
+    for (int i = 0; i < FIELDS; i++) {
+      record.put("field" + i, i);
+    }
+
+    long before = System.currentTimeMillis();
+    for (int i = 0; i < TOTAL_ITEMS; i++) {
+      producer.send(new ProducerRecord<Integer, GenericRecord>("items" + FIELDS, i, record));
     }
 
     producer.close();
+    System.out.println("Sending " + TOTAL_ITEMS + " elements took: " + (System.currentTimeMillis() - before));
   }
 
 }
